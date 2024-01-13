0440ddd01458f0d530e3ae63bfdb357e12057f9a
Reda Aoua <m.reda.aoua@gmail.com>
2024-01-13 17:40:27 +0100

    application

---
+++ src/main/java/RTG/RTG_quizapp/RtgQuizappApplication.java
@@ -0,0 +1,13 @@
+package RTG.RTG_quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtgQuizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtgQuizappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RTG/RTG_quizapp/RtgQuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package RTG.RTG_quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtgQuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
