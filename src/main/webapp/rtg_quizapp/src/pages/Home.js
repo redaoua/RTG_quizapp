beec44f4753d0914f996f1fc48898d835a5ab842
Gerzat <guillemmartinez49@gmail.com>
2024-01-22 00:50:07 +0100

    added textinput

--- src/main/webapp/rtg_quizapp/src/pages/Home.js
+++ src/main/webapp/rtg_quizapp/src/pages/Home.js
@@ -1,9 +1,24 @@
-import '../styles/tailwind.css';
+import React from 'react';
 import Button from '@mui/material-next/Button';
+import TextField from '@mui/material/TextField';
 
-
-export default function Home () {
-        return (
-            <Button>Skill Issue </Button>
-        );
+const Home = () => {
+  return (
+        <>
+        <TextField
+          required
+          id="outlined-required"
+          label="Name"
+          defaultValue=""
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <Button color="primary" size="medium" variant="elevated">
+          Start
+        </Button>
+        </>
+  );
 };
+
+export default Home;
