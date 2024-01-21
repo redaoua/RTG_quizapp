c929deb56ee142613460ff658b3a7125cb5924f5
Gerzat <guillemmartinez@sfr.fr>
2024-01-21 18:58:22 +0100

    Creation du front

---
+++ src/main/webapp/rtg_quizapp/src/pages/Home.js
@@ -0,0 +1,9 @@
+import '../styles/tailwind.css';
+import Button from '@mui/material-next/Button';
+
+
+export default function Home () {
+        return (
+            <Button>Skill Issue </Button>
+        );
+};
