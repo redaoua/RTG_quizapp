c31d4492c3dea15ae10f31067f62814a2ea36f3c
Reda Aoua <m.reda.aoua@gmail.com>
2024-01-23 09:52:38 +0100

    MAJ - QuizRepository (éviter les questions en double)

--- src/main/java/RTG/RTG_quizapp/repository/QuizRepository.java
+++ src/main/java/RTG/RTG_quizapp/repository/QuizRepository.java
@@ -3,11 +3,18 @@ package RTG.RTG_quizapp.repository;
 import RTG.RTG_quizapp.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    @Query(value = "SELECT * FROM openquizzdb ORDER BY RAND() LIMIT 10", nativeQuery = true)
-    List<Quiz> findRandomQuestions();
+    @Query(value = "SELECT DISTINCT * FROM openquizzdb ORDER BY RAND() LIMIT 100", nativeQuery = true)
+    List<Quiz> findAllQuestions();
+
+    default List<Quiz> findRandomQuestions() {
+        List<Quiz> allQuestions = findAllQuestions();
+        Collections.shuffle(allQuestions);
+        return allQuestions.stream().limit(10).collect(Collectors.toList());
+    }
 }
 
