a2d25193640e0ea0f10ea2bc9e6560e9e87ad681
Reda Aoua <m.reda.aoua@gmail.com>
2024-01-23 15:12:11 +0100

    MAJ Front - page Home

--- src/main/webapp/rtg_quizapp/src/pages/Home.js
+++ src/main/webapp/rtg_quizapp/src/pages/Home.js
@@ -1,95 +1,73 @@
-import React, {useState, useEffect} from "react";
-import Button from "@mui/material-next/Button";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import React, { useState, useEffect } from "react";
+import { Button, TextField, Box, Card, CardActions, CardContent, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-
 const Home = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [textValue, setTextValue] = useState("");
+    const [checkValue, setCheckValue] = useState(true);
 
-  const redirectToQuizPage = (event) => {
-    sessionStorage.setItem("userName", textValue)
-    navigate('/quiz');
-  };
+    useEffect(() => {
+        setCheckValue(textValue.trim() === "");
+    }, [textValue]);
 
-  const [textValue, setTextValue] = useState("");
-  const [checkValue, setCheckValue] = useState(true);
+    const redirectToQuizPage = () => {
+        sessionStorage.setItem("userName", textValue);
+        navigate('/quiz');
+    };
 
-  useEffect(() => {
-    if (textValue.trim() != ""){
-      setCheckValue(false);
-    }
-    else{
-      setCheckValue(true);
-    }
-  }, [textValue])
-
-  const card = (
-    <React.Fragment>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Entrez votre nom pour démarrer
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Box sx={{ width: "100vh", display: "flex", alignItems: "center" }}>
-          <TextField
-            required
-            id="outlined-required"
-            label="Name"
-            defaultValue=""
-            margin="normal"
-            variant="outlined"
-            sx={{ marginLeft: 1 }}
-            value={textValue}
-            onChange={(e) => setTextValue(e.target.value)}
-          />
-          <Button
-            color="primary"
-            size="medium"
-            variant="filled"
-            sx={{ marginLeft: 3, fontFamily: "arial" }}
-            onClick={redirectToQuizPage}
-            disabled={checkValue}
-          >
-            Start
-          </Button>
-        </Box>
-      </CardActions>
-    </React.Fragment>
-  );
-
-  return (
-    <>
-      <Box
-        sx={{
-          height: "100vh",
-          background:
-            "linear-gradient(126deg, rgba(5,25,85,1) 35%, rgba(63,4,117,1) 76%, rgba(5,25,85,1) 100%)",
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+    return (
         <Box
-          sx={{
-            minWidth: 275,
-            bgcolor: "#ffffff",
-            borderRadius: "6px",
-            width: "100vh",
-          }}
+            sx={{
+                height: "100vh",
+                background: "linear-gradient(126deg, rgba(5,25,85,1) 35%, rgba(63,4,117,1) 76%, rgba(5,25,85,1) 100%)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
         >
-          <Card variant="outlined">{card}</Card>
+            <Card
+                sx={{
+                    minWidth: 275,
+                    bgcolor: "#ffffff",
+                    borderRadius: "6px",
+                    width: "100vh",
+                }}
+            >
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        Entrez votre nom pour démarrer
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Box sx={{ width: "100vh", display: "flex", alignItems: "center" }}>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Nom"
+                            defaultValue=""
+                            margin="normal"
+                            variant="outlined"
+                            sx={{ marginLeft: 1 }}
+                            value={textValue}
+                            onChange={(e) => setTextValue(e.target.value)}
+                        />
+                        <Button
+                            color="primary"
+                            size="medium"
+                            variant="contained"
+                            sx={{ marginLeft: 3, fontFamily: "arial" }}
+                            onClick={redirectToQuizPage}
+                            disabled={checkValue}
+                        >
+                            Démarrer
+                        </Button>
+                    </Box>
+                </CardActions>
+            </Card>
         </Box>
-      </Box>
-    </>
-  );
+    );
 };
 
 export default Home;
+
